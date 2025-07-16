ff6654c35c120c618b8e9e0728433b5fe4f54b4b
benatkinsonx <benatkinson50@icloud.com>
2025-07-16 22:26:26 +0100

    data partitioner done

---
+++ lettuce/FedLettuce/data/data_partitioner.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import numpy as np
+from pathlib import Path
+import shutil
+
+data_dir = Path(__file__).parent
+testset_path = data_dir / 'EU_test_set.csv'
+clientdata_path = data_dir / 'clientdata'
+
+if clientdata_path.is_dir():
+    print('deleting clientdata')
+    shutil.rmtree(clientdata_path)
+
+clientdata_path.mkdir(parents=True, exist_ok=True)
+
+df = pd.read_csv(testset_path)
+informal_names = df['input_data'].tolist()
+
+num_clients = 3
+partitioned_df = np.array_split(df, num_clients)
+
+for client_id in range(num_clients):
+    client_df = partitioned_df[client_id]
+    client_csv_path = data_dir / 'clientdata' / f'client{client_id}_data.csv'
+    client_df.to_csv(client_csv_path, index=False)
+
+
